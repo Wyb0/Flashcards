bd9f013d91cbacdbdaece35b2f3127aa5e411af1
Wyb0 <andrewwybo@gmail.com>
2017-09-27 19:04:27 -0400

    added variables for basicFlash cards, console logging

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 var basicCard = require("./basiccard.js")
 var inquirer = require("inquirer")
+
+var basicFlash = [
+    new basicCard("What is 2+2?", "4"),
+    new basicCard("What is 1+1?", "2"),
+    new basicCard("What is 3+3?", "6")
+]
+//console.log(basicFlash)
+
+
+
+
+
+
